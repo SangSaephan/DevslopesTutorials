a1066f472ac29a6c5dafa2d9c16052a6367fa5fe
Sang Saephan <sangsaephan510@yahoo.com>
2017-08-27 17:53:31 -0700

    Setup custom table view cells

---
+++ DevslopesTutorials/VideoCell.h
@@ -0,0 +1,13 @@
+//
+//  VideoCell.h
+//  DevslopesTutorials
+//
+//  Created by Sang Saephan on 8/27/17.
+//  Copyright © 2017 Sang Saephan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VideoCell : UITableViewCell
+
+@end
